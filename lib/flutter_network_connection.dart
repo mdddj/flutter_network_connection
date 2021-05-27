7067529a4e5453e513bb0954a65706e835858b52
mddd <413153189@qq.com>
2021-05-27 15:51:51 +0800

    :fire: 新增方法

--- lib/flutter_network_connection.dart
+++ lib/flutter_network_connection.dart
@@ -1,11 +1,18 @@
 
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class FlutterNetworkConnection {
   static const MethodChannel _channel =
       const MethodChannel('flutter_network_connection');
+  
+  static const EventChannel _eventChannel = const EventChannel('flutter_network_connection_eventkey');
+
+  static Stream<String>? _stream;
+
 
   /// 开始采样
   static Future<void> startSampling() async {
@@ -27,11 +34,34 @@ class FlutterNetworkConnection {
     return await _channel.invokeMethod('getDownloadKBitsPerSecond');
   }
 
-  /// 开始检测
-  static Future<void> start(String address) async {
+  /// 开始检测网络url
+  ///
+  /// [address]  合法的url
+  ///
+  /// 例子：http://baidu.com
+  ///
+  /// [sourceData] 源数据，是个json字符串
+  ///
+  ///
+  static Future<void> start(String address,{ValueChanged<String>? sourceData}) async {
     var data = <dynamic,dynamic>{};
     data['address'] = address;
+    _listingStream();
+    if(_stream!=null){
+      _stream!.listen((event) {
+        print('收到流数据：$event');
+        sourceData?.call(event);
+      });
+    }
+
     await _channel.invokeMethod('startTesting',data);
+
+  }
+
+
+  /// 监听流
+  static void _listingStream(){
+   _stream = _eventChannel.receiveBroadcastStream().cast<String>();
   }
 
 }
